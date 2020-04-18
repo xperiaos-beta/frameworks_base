3ceed3d667e6d50c9d3ac92c743ba8d0092565b2
EnesSastim <sastimenes@gmail.com>
2020-04-18 17:23:57 +0000

    Revert "Add auto brightness toggle to Quick Settings"
    This reverts commit 8c15f0c7c60f735fd40e6b435cbead4ef18ac59c.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1151,7 +1151,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                             .withPlugin(QS.class)
                             .withDefault(this::createDefaultQSFragment)
                             .build());
-            mBrightnessMirrorController = new BrightnessMirrorController(mContext, mStatusBarWindow,
+            mBrightnessMirrorController = new BrightnessMirrorController(mStatusBarWindow,
                     (visible) -> {
                         mBrightnessMirrorVisible = visible;
                         updateScrimController();
