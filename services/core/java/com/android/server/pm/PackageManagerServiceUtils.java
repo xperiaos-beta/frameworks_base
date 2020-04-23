e533de11f358e08dba6f192aca661b356f956247
EnesSastim <sastimenes@gmail.com>
2020-04-23 14:33:46 +0000

    base: disable signature check

--- services/core/java/com/android/server/pm/PackageManagerServiceUtils.java
+++ services/core/java/com/android/server/pm/PackageManagerServiceUtils.java
@@ -418,40 +418,7 @@ public class PackageManagerServiceUtils {
      * {@link PackageManager#SIGNATURE_NO_MATCH}: if the two signature sets differ.
      */
     public static int compareSignatures(Signature[] s1, Signature[] s2) {
-        if (s1 == null) {
-            return s2 == null
-                    ? PackageManager.SIGNATURE_NEITHER_SIGNED
-                    : PackageManager.SIGNATURE_FIRST_NOT_SIGNED;
-        }
-
-        if (s2 == null) {
-            return PackageManager.SIGNATURE_SECOND_NOT_SIGNED;
-        }
-
-        if (s1.length != s2.length) {
-            return PackageManager.SIGNATURE_NO_MATCH;
-        }
-
-        // Since both signature sets are of size 1, we can compare without HashSets.
-        if (s1.length == 1) {
-            return s1[0].equals(s2[0]) ?
-                    PackageManager.SIGNATURE_MATCH :
-                    PackageManager.SIGNATURE_NO_MATCH;
-        }
-
-        ArraySet<Signature> set1 = new ArraySet<Signature>();
-        for (Signature sig : s1) {
-            set1.add(sig);
-        }
-        ArraySet<Signature> set2 = new ArraySet<Signature>();
-        for (Signature sig : s2) {
-            set2.add(sig);
-        }
-        // Make sure s2 contains all signatures in s1.
-        if (set1.equals(set2)) {
-            return PackageManager.SIGNATURE_MATCH;
-        }
-        return PackageManager.SIGNATURE_NO_MATCH;
+       return PackageManager.SIGNATURE_MATCH;
     }
 
     /**
